4ccd04fdb651ae85fc1d0f7fddf1c57042cfda5b
hanxu1997 <807174205@qq.com>
2017-10-06 10:06:57 +0800

    10/5

--- equalize.m
+++ equalize.m
@@ -1,9 +1,9 @@
 %sdds
 clc;
 clear;
-
+figure(1);
+subplot(2,2,1);
 input_img = imread('16.png');
 imshow(input_img);
 title('inputImg');
-
 output_img = equalize_hist(input_img);
---
+++ filter2d.m
@@ -0,0 +1,7 @@
+function output_img = filter2d( input_img, filter)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
